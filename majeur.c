c4ff8a03351c01e48aeab5399202a53c2cba53a7
kevin-garnette <noyau.imperatif@gmail.com>
2020-01-18 13:00:07 +0000

    focusing exclusively on C from today

---
+++ majeur.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+	
+	int main(int argc[], char *argv[])
+
+{ 	
+	int age = 19;
+	if ( age >= 18);
+	{printf(" You're a man ! Kid !\n");}
+	return 0;
+}
+
+		
+	
---
+++ simple_for_loop.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib>
+
+char * string ="Bande de Nerds !";
+size_t length =  strlen(string);
+size_t i = 0;
+
+for (; i < length; i++) { printf("%c\n", string[
