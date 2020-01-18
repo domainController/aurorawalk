141ae1007715474e5904da3e1a544d0566167e1c
kevin-garnette <noyau.imperatif@gmail.com>
2020-01-18 15:07:15 +0000

    understanding return 0 & argc/argv int var

---
+++ identity_check.c
@@ -0,0 +1,12 @@
+// identity_check.c
+# include <stdio.h>
+# include <stdlib.h>
+
+	int age = 42;
+	int main(int argc, char *argv[])
+{
+	if (age >= 18) {printf(" Est majeur !\n");}
+	else {printf("Est mineur !\n");}
+}
+
+
