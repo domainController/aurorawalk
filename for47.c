c362cd96e85ab0150250854aeaed49b4c6dd12a5
kevin-garnette <noyau.imperatif@gmail.com>
2020-01-20 18:55:26 +0000

    for + compteur

---
+++ for47.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+	int compteur;
+	int main(int argc[], char **argv[])
+{
+	for ( compteur = 0 ; compteur < 10 ; compteur++)
+	{ 
+		printf("vaut %d bla\n", compteur);
+	}
+}
