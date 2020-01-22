b6c78b101c224d4d65a4eb676c2b4df1b96d9764
kevin-garnette <noyau.imperatif@gmail.com>
2020-01-22 18:39:00 +0000

    My 1st function works well

--- triple.c
+++ triple.c
@@ -4,19 +4,17 @@
         int triple(int chiffre_au_hasard)
 {
 	int le_chiffre_au_cube = 0;
-	chiffre_au_hasard * 3 == chiffre_au_cube;
-	return chiffre_au_cube;
+	le_chiffre_au_cube = 3 * chiffre_au_hasard;
+	return le_chiffre_au_cube;
 }
 
 	int main (int argc[], char **argv[])
 
 {
-	int number_saisi, number_saisi_au_cube;
-	number_saisi = 0;
-	number_saisi_au_cube = 0;
+	int number_saisi = 0, number_saisi_au_cube = 0 ;
 
-	printf("Entrez un nombre : /n");
+	printf("Entrez un nombre : \n");
 	scanf("%d", &number_saisi);
-	triple(number_saisi) = number_saisi_au_cube;
-	printf("Le cube de nombre saisi est %d\n", &number_saisi_au_cube);
+	number_saisi_au_cube = triple(number_saisi);
+	printf("Ce nombre multiple par 3 donne %d\n", number_saisi_au_cube);
 }
